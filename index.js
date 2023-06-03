8e4f41fe0b055a4436522a0dc48169e419bc804c
nabi-cloud <matibag.abi@gmail.com>
2023-06-03 17:28:23 +0800

    1st Commit

---
+++ index.js
@@ -0,0 +1,4 @@
+var copy = document.querySelector(".brand-slide").cloneNode(true);
+document.querySelector(".brands").appendChild(copy);
+var copy = document.querySelector(".brand-slide").cloneNode(true);
+document.querySelector(".brands").appendChild(copy);
